76162a578b6e8bec8ad6d54eab24cfc02c969240
Eslam Shaker <binshakerr@gmail.com>
2023-04-05 02:46:43 +0400

    scrape all pages

--- scraper.py
+++ scraper.py
@@ -35,6 +35,28 @@ def scrape_quotes(url):
     
     return results
 
+
+def scrape_all_quotes():
+    results = []
+    i = 1
+    while True:
+        try:
+            url = "https://www.goodreads.com/quotes?page=" + str(i)
+            sub_results = scrape_quotes(url)
+            print("scraping page: " + str(i))
+            # Goodreads page size = 30, and it duplicates last page results after reaching end of pages 
+            if results[-30:] == sub_results:
+                break 
+            results += sub_results
+            i+= 1
+        except Exception as e:
+            print(e)
+            break 
+
+
+    return results
+
+
 if __name__ == "__main__":
-    results = scrape_quotes("https://www.goodreads.com/quotes")
-    print(results)
+   result = scrape_all_quotes()
+   print(result[99])
